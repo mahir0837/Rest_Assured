6d5dc527a0a22d6329bf636319d290ce13c4e15b
mahir0837 <mahirsarac89@gmail.com>
2024-02-12 10:54:01 -0500

    P02 test are done

--- src/test/java/com/cydeo/pojo/Spartan.java
+++ src/test/java/com/cydeo/pojo/Spartan.java
@@ -1,5 +1,6 @@
 package com.cydeo.pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@JsonIgnoreProperties(value = "id",allowSetters = true)
 public class Spartan {
 
     private int id;
