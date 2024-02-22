d505a94af9648f783aef3bcc04ae66fb79f5a3e6
mahir0837 <mahirsarac89@gmail.com>
2024-02-22 10:03:44 -0500

    excell test added

--- src/test/java/com/cydeo/day11/P07_ExcellUtilTest.java
+++ src/test/java/com/cydeo/day11/P07_ExcellUtilTest.java
@@ -1,2 +1,11 @@
-package com.cydeo.day11;public class P07_ExcellUtilTest {
+package com.cydeo.day11;
+
+import org.junit.jupiter.api.Test;
+
+public class P07_ExcellUtilTest {
+
+    @Test
+    void test1() {
+
+    }
 }
