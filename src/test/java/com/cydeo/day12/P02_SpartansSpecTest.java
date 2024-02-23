673b34bf327af2e5a99f1ff927927fe334389312
mahir0837 <mahirsarac89@gmail.com>
2024-02-23 14:18:25 -0500

    P02 test is done

--- src/test/java/com/cydeo/day12/P02_SpartansSpecTest.java
+++ src/test/java/com/cydeo/day12/P02_SpartansSpecTest.java
@@ -1,4 +1,110 @@
 package com.cydeo.day12;
 
-public class P02_SpartansSpecTest extends Spartan{
+import com.cydeo.utilities.SpartanNewTestBase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class P02_SpartansSpecTest extends SpartanNewTestBase {
+
+    @Test
+    public void getAllSpartans(){
+        given()
+                .accept(ContentType.JSON)
+                .auth()
+                .basic("admin","admin")
+        .when()
+                .get("/spartans")
+        .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON);
+
+    }
+
+    @Test
+    public void getAllSpartansWithRequestAndResponseSpect(){
+        given().
+                spec(requestSpec)
+        .when()
+                .get("spartans")
+        .then()
+                .spec(responseSpec);
+    }
+
+    @Test
+    void getOneSpartansWithRequestAndResponseSpect() {
+        given()
+                .spec(requestSpec)
+                .and()
+                .pathParam("id",10)
+        .when()
+                .get("spartans/{id}")
+        .then()
+                .spec(responseSpec)
+                .body("id",equalTo(10));
+    }
+
+    @Test
+    void getOneSpartansAsAUser() {
+        given()
+                .spec(dynamicRequestSpec("user"))
+                .and()
+                .pathParam("id",10)
+                .when()
+                .get("spartans/{id}")
+                .then()
+                .spec(dynamicResponseSpec(200))
+                .body("id",equalTo(10));
+    }
+
+    /**
+     *  Create GET_RBAC.csv
+     *    username,password,id,statusCode
+     *    admin,admin,3,200
+     *    editor,editor,3,200
+     *    user,user,3,200
+     *
+     *  Create a parameterized test to check RBAC for GET method
+     *
+     *
+     */
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/GET_RBAC.csv",numLinesToSkip = 1)
+    public void getSingleSpartan_GET_RBAC(String role,int id,int statusCode){
+      given()
+              .spec(dynamicRequestSpec(role))
+              .pathParam("id",id)
+      .when()
+              .get("/spartans/{id}")
+      .then()
+              .spec(dynamicResponseSpec(statusCode));
+    }
+    /**
+     *  Create DELETE_RBAC.csv
+     *   username,password,id,statusCode
+     *    editor,editor,3,403
+     *    user,user,3,403
+     *    admin,admin,3,204
+     *
+     *  Create a parameterized test to check RBAC for DELETE method
+     *
+     *
+     */
+    @ParameterizedTest
+    @CsvFileSource(resources = "/DELETE_RBAC.csv",numLinesToSkip = 1)
+    public void deleteSingleSpartan_DELETE_RBAC(String role, int id, int statusCode){
+
+        given().spec(dynamicRequestSpec(role))
+                .pathParam("id",id)
+                .when().delete("/spartans/{id}")
+                .then().spec(dynamicResponseSpec(statusCode));
+
+    }
+
 }
--- src/test/java/com/cydeo/utilities/SpartanNewTestBase.java
+++ src/test/java/com/cydeo/utilities/SpartanNewTestBase.java
@@ -1,14 +1,71 @@
 package com.cydeo.utilities;
 
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.BeforeAll;
 
 import static io.restassured.RestAssured.*;
 
 public class SpartanNewTestBase {
+
+    public static RequestSpecification requestSpec;
+    //    public static RequestSpecification requestUserSpec;
+    public static ResponseSpecification responseSpec;
+    private static String username;
+    private static String password;
     @BeforeAll
-    public static void init(){
-        baseURI="http://54.208.32.156";
-        port=7000;
-        basePath="/api";
+    public static void init() {
+        baseURI = "http://54.208.32.156";
+        port = 7000;
+        basePath = "/api";
+
+
+
+//        requestSpec = given()
+//                .accept(ContentType.JSON)
+//                .auth()
+//                .basic("admin", "admin");
+
+//        requestUserSpec = given()
+//                .accept(ContentType.JSON)
+//                .auth()
+//                .basic("user", "user");
+
+//        responseSpec =
+//                expect().statusCode(200)
+//                        .contentType(ContentType.JSON);
+    }
+
+    public static RequestSpecification dynamicRequestSpec(String role) {
+        try {
+            switch (role) {
+                case "admin":
+                    username = "admin";
+                    password = "admin";
+                    break;
+                case "user":
+                    username = "user";
+                    password = "user";
+                    break;
+                case "editor":
+                    username = "editor";
+                    password = "editor";
+                    break;
+            }
+        }catch (Exception e){
+            System.out.println("No Such a user type ");
+            e.printStackTrace();
+        }
+        return requestSpec = given()
+                .accept(ContentType.JSON)
+                .auth()
+                .basic(username, password);
+    }
+    public static ResponseSpecification dynamicResponseSpec(int statusCode) {
+
+        return responseSpec =
+                expect().statusCode(statusCode)
+                        .contentType(ContentType.JSON);
     }
 }
