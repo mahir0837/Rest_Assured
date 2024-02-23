30906eb03589a89ff234b73f3ac24a842fbc28c2
mahir0837 <mahirsarac89@gmail.com>
2024-02-23 11:54:52 -0500

    Old way and new way comperasion

---
+++ src/test/java/com/cydeo/day12/P01_OldRestAssured.java
@@ -0,0 +1,47 @@
+package com.cydeo.day12;
+
+import com.cydeo.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class P01_OldRestAssured extends SpartanTestBase {
+
+    @Test
+    void getAllSpartan() {
+        given()
+                .accept(ContentType.JSON)
+                .auth()
+                .basic("admin","admin")
+        .when()
+                .get("/spartans")
+        .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id[0]",equalTo(1))
+                .body("id[1]",equalTo(2))
+                .log().body();
+    }
+
+    @Test
+    void getAllSpartansOldWay() {
+        given()
+                .accept(ContentType.JSON)
+                .auth().basic("admin","admin")
+        .expect()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id[0]",equalTo(1))
+                .body("id[1]",equalTo(2))
+        .when()
+                .get("/spartans");
+    }
+    /*
+        OLD WAY -->EXPECT()
+            -It works like a soft assertion
+        NEW WAY -->then() (this is one that we are using)
+            -It works like hard assertion
+     */
+}
--- src/test/java/com/cydeo/utilities/SpartanTestBase.java
+++ src/test/java/com/cydeo/utilities/SpartanTestBase.java
@@ -1,13 +1,15 @@
 package com.cydeo.utilities;
 
-import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
+import static io.restassured.RestAssured.*;
 
 public class SpartanTestBase{
 
     @BeforeAll
     public static void init(){
-        RestAssured.baseURI="http://54.208.32.156:8000";
+        baseURI="http://54.208.32.156";
+        port=7000;
+        basePath="/api";
     }
 
 }
