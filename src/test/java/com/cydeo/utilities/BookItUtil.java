1a0c9b3c3ba7616fc72173816ef60ee48568b49f
mahir0837 <mahirsarac89@gmail.com>
2024-02-23 14:53:05 -0500

    getTokenByRole method added!!

--- src/test/java/com/cydeo/utilities/BookItUtil.java
+++ src/test/java/com/cydeo/utilities/BookItUtil.java
@@ -2,8 +2,12 @@ package com.cydeo.utilities;
 
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -22,4 +26,50 @@ public class BookItUtil {
         return "Bearer " + jsonPath.getString("accessToken");
 
     }
+    //teacher / student-member / student-leader
+    public static String getTokenByRole(String role) {
+        String email = "";
+        String password = "";
+
+        switch (role) {
+            case "teacher":
+                email = ConfigurationReader.getProperty("teacher_email");
+                password = ConfigurationReader.getProperty("teacher_password");
+                break;
+
+            case "student-member":
+                email = ConfigurationReader.getProperty("team_member_email");
+                password = ConfigurationReader.getProperty("team_member_password");
+                break;
+            case "student-leader":
+                email = ConfigurationReader.getProperty("team_leader_email");
+                password = ConfigurationReader.getProperty("team_leader_password");
+                break;
+            default:
+
+                throw new RuntimeException("Invalid Role Entry :\n>> " + role +" <<");
+        }
+
+        Map<String, String> credentials = new  HashMap<>();
+        credentials.put("email", email);
+        credentials.put("password", password);
+
+        String accessToken = given()
+                .queryParams(credentials)
+                .when().get( "/sign").path("accessToken");
+
+        return  "Bearer " + accessToken;
+
+    }
+
+    //Create one req spec for bookitUtils which accepts role
+    public static RequestSpecification getReqSpec(String role){
+
+        return given().
+                log().all()
+                .accept(ContentType.JSON)
+                .header("Authorization", getTokenByRole(role));
+    }
+
+
 }
